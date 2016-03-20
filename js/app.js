1a1fe49a56b91b854fbc867da8f27df51f115608
ilterates <iltates@gmail.com>
2016-03-19 19:32:48 -0700

    visually working tic tac toe- doesn't keep count or check for winner

--- js/app.js
+++ js/app.js
@@ -2,5 +2,33 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+var $turn = 2;
+
+  $(this).find(".box").click( function player1(event){
+    if ($(this).text() === ""){
+        if ($turn %2 === 0){
+        $(this).text("x");
+      } else {
+        $(this).text("O");
+      }
+    $turn = $turn + 1;
+
+    }
+    else {
+        console.log("there is text");
+        $("div").effect( "shake",{times:2}, 100  );
+    } 
+  });
+
+/*  $(".refresh").on("mouseover", function dude(){
+      $(".refresh").animate({
+
+        "font-size": "60px"
+
+
+      }, 1000);
+
+  }); */
+
 
 });
