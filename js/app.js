4b3b615519388c514fcd2983908f699db056fee5
ilterates <iltates@gmail.com>
2016-03-19 22:06:54 -0700

    working on tie

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 var $turn = 2;
-
-  $(this).find(".box").click( function player1(event){
+var $box = $(".box");
+var $topLeft = $("#topLeft");
+var $topMid = $("topMid");
+  $(this).find($box).click( function game(event){
     if ($(this).text() === ""){
         if ($turn %2 === 0){
-        $(this).text("x");
+        $(this).text("X");
       } else {
         $(this).text("O");
       }
@@ -16,10 +18,25 @@ var $turn = 2;
     }
     else {
         console.log("there is text");
-        $("div").effect( "shake",{times:2}, 100  );
-    } 
+        $("div").effect( "shake",{times:2}, 270  );
+    }
   });
 
+  $(this).find($box).click( function tie(event){
+    if ($("#topLeft").text() === "X" || $("#topLeft").text() === "O" &&
+        $("#topMid").text() === "X" || $("#topMid").text() ===  "O" &&
+        $("#topRight").text() === "X" || $("#topRight").text() === "O" &&
+        $("#midLeft").text() === "X" || $("#midLeft").text() === "O" &&
+        $("#midMid").text() === "X" || $("#midMid").text() === "O" &&
+        $("#midRight").text() === "X" || $("#midRight").text() === "O" &&
+        $("#botLeft").text() === "X" || $("#botLeft").text() === "O" &&
+        $("#botMid").text() === "X" || $("#botMid").text() === "O" &&
+        $("#botRight").text() === "X" || $("#botRight").text() === "O" ) {
+    console.log("TIE");
+  } else {
+    console.log("not tie");
+  }
+});
 /*  $(".refresh").on("mouseover", function dude(){
       $(".refresh").animate({
 
