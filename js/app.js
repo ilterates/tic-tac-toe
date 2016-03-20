c4a46f41020b4a175e0ddacdb38d71c119829959
ilterates <iltates@gmail.com>
2016-03-20 00:11:12 -0700

    Working on X or O winning. buggy

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ var $turn = 2;
 var $box = $(".box");
 var $topLeft = $("#topLeft");
 var $topMid = $("topMid");
+var $topRight = $("topRight");
+var $midLeft = $("midLeft");
+var $midMid = $("midMid");
+var $midRight = $("midRight");
+var $botLeft = $("botLeft");
+var $botMid = $("botMid");
+var $botRight = $("botRight");
   $(this).find($box).click( function game(event){
     if ($(this).text() === ""){
         if ($turn %2 === 0){
@@ -14,12 +21,27 @@ var $topMid = $("topMid");
         $(this).text("O");
       }
     $turn = $turn + 1;
-
     }
     else {
         console.log("there is text");
         $("div").effect( "shake",{times:2}, 270  );
     }
+    // SETTING WINS FOR X
+    if ($("#topLeft") && $("#topMid") && $("#topRight").is(':contains("X")') ||
+        $("#midLeft") && $("#midMid") && $("#midRight").is(':contains("X")') ||
+        $("#botLeft") && $("#botMid") && $("#botRight").is(':contains("X")') ||
+        $("#topLeft") && $("#midMid") && $("#botRight").is(':contains("X")') ||
+        $("#topRight") && $("#midMid") && $("#botLeft").is(':contains("X")')) {
+          console.log("X WON");
+       }
+    // SETTING WINS FOR O
+    if ($("#topLeft") && $("#topMid") && $("#topRight").is(':contains("O")') ||
+        $("#midLeft") && $("#midMid") && $("#midRight").is(':contains("O")') ||
+        $("#botLeft") && $("#botMid") && $("#botRight").is(':contains("O")') ||
+        $("#topLeft") && $("#midMid") && $("#botRight").is(':contains("O")') ||
+        $("#topRight") && $("#midMid") && $("#botLeft").is(':contains("O")')) {
+          console.log("O WON");
+       }
   });
 
   $(this).find(".box").click( function gameOver(){
